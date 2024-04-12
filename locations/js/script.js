6942fe97f5112399dd0c5e13f3619f7d6d1cb37c
CyrilleVerleye <160618056+CyrilleVerleye@users.noreply.github.com>
2024-04-12 15:45:11 +0200

    header done 99%

--- locations/js/script.js
+++ locations/js/script.js
@@ -3,13 +3,36 @@ function hamburgerMenu() {
 	const nav = document.getElementById("nav");
 	const body = document.body;
 	const main = document.querySelector("main");
+	const headerButton = document.getElementById("header-button");
 
 	hamburger.addEventListener("click", function () {
 		hamburger.classList.toggle("hamburger-active");
 		nav.classList.toggle("nav-active");
 		body.classList.toggle("no-scroll");
 		main.classList.toggle("filter");
+		headerButton.classList.toggle("header-button-active");
 	});
 }
 
 hamburgerMenu();
+
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", function () {
+	let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+	if (currentScroll > lastScrollTop) {
+		// Scrolling down
+		document.querySelector("header").classList.remove("sticky");
+	} else {
+		// Scrolling up
+		document.querySelector("header").classList.add("sticky");
+	}
+
+	// Check if the user has scrolled to the top
+	if (currentScroll <= 0) {
+		document.querySelector("header").classList.remove("sticky");
+	}
+
+	lastScrollTop = currentScroll;
+});
